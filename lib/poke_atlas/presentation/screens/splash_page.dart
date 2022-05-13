af7bfe8c5987c13879fd1e3a34972e810e8b16fc
Gabriel Luca <gabrieluca17@gmail.com>
2022-05-13 16:50:50 -0300

    feat: Add home and menu pages

---
+++ lib/poke_atlas/presentation/screens/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/home_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
