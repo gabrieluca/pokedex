1b49daed83d497e0e4566fd8bf33303ea6d03be1
Gabriel Luca <gabrieluca17@gmail.com>
2022-05-13 11:52:39 -0300

    feat: Add assets & typography

---
+++ lib/poke_atlas/presentation/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const searchPokemonButtonColor = Color.fromRGBO(54, 54, 54, 1);
+  static const backgroundColor = Color.fromRGBO(44, 44, 45, 0.9);
+  static const searchButtonColor = Color.fromRGBO(255, 255, 255, 0.1);
+}
+
+class TextConstants {
+  static const appTitle = 'PokeAtlas';
+  static const homeTitle = 'Pokedex';
+  static const homeBody =
+      'Use the Advanced Search\n to explore Pokémon by\n type, weakness,\n Ability, and more!\n Search for a Pokémon by\n name or using its\n National Pokédex\n number.';
+  static const searchButtonName = 'Search Pokemon';
+}
