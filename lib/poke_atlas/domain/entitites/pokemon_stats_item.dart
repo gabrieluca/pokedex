dccae3c51f9bc3ca79ec9c90c01af1666077a01f
Gabriel Luca <gabrieluca17@gmail.com>
2022-10-29 15:03:53 -0300

    feat: Add Pokemon entities

---
+++ lib/poke_atlas/domain/entitites/pokemon_stats_item.dart
@@ -0,0 +1,11 @@
+class PokemonStatsItemEntity {
+  final String name;
+  final int baseStat;
+  final int effort;
+
+  const PokemonStatsItemEntity({
+    required this.name,
+    required this.baseStat,
+    required this.effort,
+  });
+}
