1ee69aaaac1a92ca1e5b264942b385181a2b7c3b
Gabriel Luca <gabrieluca17@gmail.com>
2022-10-29 15:03:53 -0300

    feat: Add assets & typography

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'poke_atlas/core/dependency_injection.dart';
-import 'poke_atlas/presentation/pages/splash_page.dart';
+import 'poke_atlas/presentation/utils/constants.dart';
+import 'poke_atlas/presentation/screens/splash_page.dart';
 
 void main() {
   Injection.registerDependencies();
@@ -15,11 +16,64 @@ class PokeAtlasApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Poke Atlas',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: TextConstants.appTitle,
+      theme: _appTheme,
       home: const SplashPage(),
     );
   }
 }
+
+const _appFontFamily = 'FiraCode';
+const _textColor = Colors.white;
+
+final _appTheme = ThemeData(
+  brightness: Brightness.dark,
+  fontFamily: _appFontFamily,
+  textTheme: const TextTheme(
+    headlineLarge: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 30,
+      fontWeight: FontWeight.w700,
+      color: _textColor,
+    ),
+    headlineMedium: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 25,
+      fontWeight: FontWeight.w700,
+      color: _textColor,
+    ),
+    headlineSmall: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: _textColor,
+    ),
+    titleLarge: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 20,
+      color: _textColor,
+    ),
+    titleMedium: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: _textColor,
+    ),
+    titleSmall: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: _textColor,
+    ),
+    bodyLarge: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 16,
+      color: _textColor,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: _appFontFamily,
+      fontSize: 14,
+      color: _textColor,
+    ),
+  ),
+);
